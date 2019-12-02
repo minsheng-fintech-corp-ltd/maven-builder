c6f8dc1d560ffc9e0442bd0aab7aa64427e3822d
Fan Shang Xiang <fanshangxiang@gmail.com>
2019-12-02 10:39:09 +0800

    update image mirror

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-docker push $REPO:$COMMIT
-docker tag $REPO:$COMMIT $REPO:latest
-docker push $REPO:latest
+docker push $REPO:java8-$COMMIT
+docker tag $REPO:java8-$COMMIT $REPO:java8-latest
+docker push $REPO:java8-latest
+docker push $REPO:java11-$COMMIT
+docker tag $REPO:java11-$COMMIT $REPO:java11-latest
+docker push $REPO:java11-latest
