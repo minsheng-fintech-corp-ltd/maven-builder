e940ed45dc7a757d329bed7672ccd982acac887b
Fan Shang Xiang <fanshangxiang@gmail.com>
2020-02-20 20:27:07 +0800

    add variable for build pack version

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-docker push $REPO:java8-$COMMIT
-docker tag $REPO:java8-$COMMIT $REPO:java8-latest
-docker push $REPO:java8-latest
-docker push $REPO:java11-$COMMIT
-docker tag $REPO:java11-$COMMIT $REPO:java11-latest
-docker push $REPO:java11-latest
+docker push $REPO:java8-${BUILDPACK_LIFECYCLE}-$COMMIT
+docker tag $REPO:java8-${BUILDPACK_LIFECYCLE}-$COMMIT $REPO:java8-${BUILDPACK_LIFECYCLE}-latest
+docker push $REPO:java8-${BUILDPACK_LIFECYCLE}-latest
+docker push $REPO:java11-${BUILDPACK_LIFECYCLE}-$COMMIT
+docker tag $REPO:java11-${BUILDPACK_LIFECYCLE}-$COMMIT $REPO:java11-${BUILDPACK_LIFECYCLE}-latest
+docker push $REPO:java11-${BUILDPACK_LIFECYCLE}-latest
